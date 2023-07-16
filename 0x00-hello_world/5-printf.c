05bc1434e3a16b2284d8b5b082ed7ac6fb011014
omar ezzat <omarezzat.m2002@gmail.com>
2023-07-16 16:45:33 +0300

    ha

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
 #include<stdio.h>
 /**
- *main - Entry point 
- *
- *Descrption: A C program that prints with printf function 
- *
- * Return: Always 0 (Success)
+*main - Entry point 
+*
+*Descrption: A C program that prints with printf function 
+*
+* Return: Always 0 (Success)
 */
 int main(void)
 {
