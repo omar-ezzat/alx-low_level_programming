69dc5a1d812bb75fffb27f98cefcdb3bd0d1dfe1
omar ezzat <omarezzat.m2002@gmail.com>
2023-07-16 17:40:09 +0300

    haaaaaa

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,7 +1,14 @@
 #include<stdio.h>
 #include<unistd.h>
-
-int main(void){
-	write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n" ,59);
+/**
+ *mian - entry point
+ *
+ *Description: print in other method
+ *
+ *Return: 0 succss
+*/
+int main(void)
+{
+	write(2 , "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n" , 59);
 	return (1);
 }
