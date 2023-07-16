fbe0403fca2e8fc3917b2073371c17c74c7d9214
omar ezzat <omarezzat.m2002@gmail.com>
2023-07-16 17:06:14 +0300

    ha

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ *mian - Entry point
+ *
+ *Description: using sizeof fun
+ * 
+ * return: 0 success
+*/
+int main(void)
+{
+	printf("size of a char: %d byts(s)\n", sizeof(char));
+	printf("size of a int: %d byts(s)\n", sizeof(int));
+	printf("size of a long int: %d byts(s)\n", sizeof(long int));
+	printf("size of a long long int: %d byts(s)\n", sizeof(long long int));
+	printf("size of a float: %d byts(s)\n", sizeof(float));
+	return (0);
+
+}
+
