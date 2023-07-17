baff34ec1506971775aef7bdbd4fb57891c2c6d2
omar ezzat <omarezzat.m2002@gmail.com>
2023-07-17 17:34:13 +0300

    see

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ *main - Entry piont
+ *
+ *Description: print numbers base 10
+ *
+ *Return: 0 (success)
+ *
+*/
+int main(void)
+{
+	int n = 0;
+
+	while (n < 10)
+	{
+		putchar(n + '0');
+		n++;
+	}
+	putchar('\n');
+
+	return (0);
+}
