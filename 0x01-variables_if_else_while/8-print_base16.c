319d001144a4f78b758c63ec5e6fc697631743bb
omar ezzat <omarezzat.m2002@gmail.com>
2023-07-17 17:52:18 +0300

    see

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ *main - Entry piont
+ *
+ *Description: use putchar fun to print all alphabet
+ *
+ *Return: 0 (success)
+ *
+*/
+int main(void)
+{
+	char ch = '0';
+
+	while (ch <= 'f')
+	{
+		putchar(ch);
+		ch++;
+	}
+	putchar('\n');
+
+	return (0);
+}
