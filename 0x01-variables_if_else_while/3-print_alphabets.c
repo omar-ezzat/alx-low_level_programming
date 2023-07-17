c2a704eff9766b094c40676722cf3f8ad338867b
omar ezzat <omarezzat.m2002@gmail.com>
2023-07-17 17:05:31 +0300

    see

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+ *main - Entry piont
+ *
+ *Description: use putchar fun to print all alphabet
+ *
+ *Return: 0 (success)
+ *
+*/
+int main(void)
+{
+	char ch = 'a';
+        char CH = 'A';
+
+	while (ch <= 'z')
+	{
+		putchar(ch);
+		ch++;
+	}
+
+	while (CH <= 'Z')
+	{
+		putchar(CH);
+		CH++;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
