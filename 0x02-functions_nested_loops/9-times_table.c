a780a25801c511a1f05e48ae23cc1267cbb159f1
omar ezzat <omarezzat.m2002@gmail.com>
2023-07-19 12:16:48 +0300

    task 9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,25 @@
+#include"main.h"
+/**
+ * times_table - function print the 9 time table
+ */
+void times_table(void)
+{
+	int row = 0;
+	int col = 0;
+	int mul = 0;
+
+	for (row = 0; row < 10; row++)
+	{
+		for (col = 0; col < 10; col++)
+		{
+			mul = row * col;
+			_putchar('0' + mul);
+			if (col != 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+			}
+		}
+	}
+}
