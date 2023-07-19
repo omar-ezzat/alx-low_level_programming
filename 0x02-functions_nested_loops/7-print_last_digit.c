053c878695522e2b5f9a8dbf68f17b3b10e70ba1
omar ezzat <omarezzat.m2002@gmail.com>
2023-07-19 11:28:17 +0300

    task 7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -14,11 +14,10 @@ int print_last_digit(int n)
 	if (n >= 0)
 	{
 		lastdigit = n % 10;
-		
 	}
 	else
 	{
-		lastdigit = -n % 10;
+		lastdigit = ((-1 * n) % 10);
 	}
 	_putchar(lastdigit + '0');
 	return (lastdigit);
