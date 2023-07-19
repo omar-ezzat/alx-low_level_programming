033e9ea80fdce75bcecb50b4fb74a0b81b3a8d09
omar ezzat <omarezzat.m2002@gmail.com>
2023-07-19 10:38:17 +0300

    task 6

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,14 +1,13 @@
 #include"main.h"
 /**
- *
- *_abs - function turn the number to the absolute
+ * _abs - function turn the number to the absolute
  *
  *@n: is the input number
- *Return: the absolute value of input number 
+ *Return: the absolute value of input number
 */
 int _abs(int n)
 {
-	if (n >=0)
+	if (n >= 0)
 		return (n);
 	else
 		return (-n);
