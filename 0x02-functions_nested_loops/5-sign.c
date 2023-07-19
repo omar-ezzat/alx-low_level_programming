bf90036138d9fe5823932265e9d0400d1363622e
omar ezzat <omarezzat.m2002@gmail.com>
2023-07-19 10:35:44 +0300

    task 5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,6 +1,7 @@
 #include"main.h"
 /**
- *print_sign - print + if n is greater than zero, 0 if n is zero and - if n less than zero.
+ *print_sign - print + if n is greater than zero,
+ *  0 if n is zero and - if n less than zero.
  *
  *@n: takes integer type input for function.
  *
