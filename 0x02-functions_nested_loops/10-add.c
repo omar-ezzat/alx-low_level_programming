dc3f8deef4c7c45ecb584ea8f42cfd5841375118
omar ezzat <omarezzat.m2002@gmail.com>
2023-07-19 13:36:26 +0300

    task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include"main.h"
+/**
+ * add - function add two numbers
+ * @a: first input number
+ * @b: second input number
+ * Return: the sumation
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
