2f912ea6784b0e12637ec47a74f7c98c62c0d80e
omar ezzat <omarezzat.m2002@gmail.com>
2023-07-21 13:26:37 +0300

    task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include"main.h"
+
+/**
+ * _isupper - check if c is upper
+ *
+ * @c: input alphabet
+ *
+ * Return: 1 if the uppercase. 0 if not
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+		_putchar('0'+i);
+	_Putchar('\n');
+}
