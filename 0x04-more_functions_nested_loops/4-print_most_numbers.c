5467224f44795152b1f6b1cb646576b232f287ea
omar ezzat <omarezzat.m2002@gmail.com>
2023-07-21 15:17:46 +0300

    task 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -5,17 +5,13 @@
  *
  * Return: void
  */
-
 void print_most_numbers(void)
 {
-	int i;
-
-	for (i = '0'; i <= '9'; i++)
-	{
-		if (i != '2' && i != '4')
-		{
-			_putchar(i);
-		}
-	}
-	_putchar('\n');
+char c;
+for (c = '0'; c <= '9'; c++)
+{
+if (!(c == '2' || c == '4'))
+_putchar(c);
+}
+_putchar('\n');
 }
