a64da5d7d5de100aa9a9c64f7b24a12473d3a25c
omar ezzat <omarezzat.m2002@gmail.com>
2023-07-21 13:18:55 +0300

    task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include"main.h"
+/**
+ * mul - function multiplies two integers 
+ *
+ * @a: input first num
+ * @b: input second num
+ *
+ * Return: 1 if the uppercase. 0 if not
+ */
+int mul(int a, int b)
+{
+	return (a*b);
+}
