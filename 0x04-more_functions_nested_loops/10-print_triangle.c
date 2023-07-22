92d474c38e8fdbc67d871a53d84c7da02ec3a7c4
omar ezzat <omarezzat.m2002@gmail.com>
2023-07-22 05:01:30 +0300

    task 10

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,28 @@
+#include"main.h"
+/**
+ * print_triangle - prints a triangle
+ * @size: size of the triangle
+ *
+ * Return: always 0 (success)
+*/
+void print_triangle(int size)
+{
+	int hight, base;
+
+	if (size <= 0)
+		_putchar('\n');
+	else
+	{
+		for (hight = 1; hight <= size; hight++)
+		{
+			for (base = 1; base <= size; base++)
+			{
+				if((hight + base) <= size)
+					_putchar(' ');
+				else
+					_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+}
