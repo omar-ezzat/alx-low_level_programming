bacbca3ecb1d1af6a6d5aa6679991d7956f4790a
omar ezzat <omarezzat.m2002@gmail.com>
2023-07-24 17:29:55 +0300

    task 9

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,30 @@
+#include"main.h"
+/**
+ * _atoi - convert a string to an integer.
+ * @s: string
+ * Return: integer number
+ */
+int _atoi(char *s)
+{
+	int num = 0;
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			num += s[i] - '0';
+			num *= 10;
+		}
+		if (s[i] == '-')
+		{
+			if (num > 0)
+				num *= -1;
+		}
+		if (s[i] == '+')
+		{
+			if (num < 0)
+				num *= -1;
+		}
+	}
+}
