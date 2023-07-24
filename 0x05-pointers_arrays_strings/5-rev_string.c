0e864502e69bc2a4a6f3e3c67e682e74bd9a8bcd
omar ezzat <omarezzat.m2002@gmail.com>
2023-07-24 14:16:23 +0300

    task 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,21 @@
+#include"main.h"
+/**
+ * rev_string - reverses a string
+ * @s: pointer to string
+ * Return: void
+ */
+void rev_string(char *s)
+{
+	int i, j;
+	char temp;
+
+	while (s[j])
+		j++;
+	for (i = 0; i < j; i++)
+	{
+		j--;
+		temp = s[i];
+		s[i] = s[j];
+		s[j] = temp;
+	}
+}
