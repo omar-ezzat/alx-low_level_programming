fe733f44cfca9ea7526f5a8264ed7fb5362dd6d8
omar ezzat <omarezzat.m2002@gmail.com>
2023-07-24 14:27:24 +0300

    task 6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,16 @@
+#include"main.h"
+/**
+ * puts2 - prints every other character
+ * @str: pointer to string
+ * Return: void
+ */
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i+=2)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
