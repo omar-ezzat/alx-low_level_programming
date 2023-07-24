a628ee7a57f93ce18ecc66f8bdaada7508dcd1df
omar ezzat <omarezzat.m2002@gmail.com>
2023-07-24 18:15:02 +0300

    task 100

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -16,7 +16,7 @@ int _atoi(char *s)
 			sign *= -1;
 		else if (s[i] >= '0' && s[i] <= '9')
 		{
-			num = (num * 10) + (s[i] - '0');
+			num = ( num * 10 ) + (s[i] - '0');
 		}
 		else if(num > 0)
 			break;
