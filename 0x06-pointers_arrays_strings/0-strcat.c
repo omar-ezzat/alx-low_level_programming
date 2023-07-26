515730e860f2f6ead4be482b7fce8620e3e42eea
omar ezzat <omarezzat.m2002@gmail.com>
2023-07-26 09:52:10 +0300

    task 0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,23 @@
+#include"main.h"
+/**
+ * _strcat - concatenates two strings
+ * @dest: first string
+ * @src: second sting
+ * Return: pointer to char
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i = 0;
+	int j = 0;
+
+	while (dest[i])
+	{
+		i++;
+	}
+	for (j = 0; src[j] != '\0'; j++)
+	{
+		dest[i] = src[j];
+		i++;
+	}
+	return dest;
+}
