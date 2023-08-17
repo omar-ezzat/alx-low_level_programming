aaed0d65f8233ec2e1b98912869061597e64ac20
omar ezzat <omarezzat.m2002@gmail.com>
2023-08-17 03:13:39 +0300

    k

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - a dog 
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner
+ * Description: just dog struct
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+#endif
